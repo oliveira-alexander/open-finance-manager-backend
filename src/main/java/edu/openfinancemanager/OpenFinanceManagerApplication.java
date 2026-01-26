d0a5848b4e12fe2de489f3960a15ee6c083c0c35
oliveira-alexander <marzinho.alex@gmail.com>
2026-01-25 21:40:57 -0300

    Commit inicial

---
+++ src/main/java/edu/openfinancemanager/OpenFinanceManagerApplication.java
@@ -0,0 +1,13 @@
+package edu.openfinancemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFinanceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenFinanceManagerApplication.class, args);
+	}
+
+}
